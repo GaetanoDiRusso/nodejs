f5a7f9ea34f084fc0966bbf5880bb0089b35b672
GaetanoDiRusso <92637619+GaetanoDiRusso@users.noreply.github.com>
2025-08-11 13:54:22 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,9 @@ router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
+// Serve the index.html file for the root route
+router.get('/health', (req, res) => {
+  res.status(200).json({ message: "Server running!" })
+});
+
 module.exports = router;
